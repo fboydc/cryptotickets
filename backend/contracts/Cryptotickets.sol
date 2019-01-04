3486a72373a70d71ea56a4903c59d72d2e871cb3
fboydc <fboydc@gmail.com>
2019-01-04 11:53:18 -0500

    latest

--- backend/contracts/Cryptotickets.sol
+++ backend/contracts/Cryptotickets.sol
@@ -14,20 +14,9 @@ contract Cryptotickets is TradeableERC721Token {
     using Strings for *;
 
     address proxyRegistryAddress;
-    string api_endpoint = "https://nuefwqsdv3.execute-api.us-east-1.amazonaws.com/testing/cryptotickets/{proxy+}/?";
 
     constructor(address _proxyRegistryAddress) TradeableERC721Token("Cryptoticket", "CTKT", _proxyRegistryAddress) public {
         proxyRegistryAddress = _proxyRegistryAddress;
-    }
-
-    /***********************************************************
-    *@name: tokenURI
-    *@parameters: unique ticket ID (uint256), unique event ID (uint256)    
-    *Description: 
-    *returns the URI for the related token metadata  (string)
-    ************************************************************/
-    function tokenURI(uint256 _tokenId, uint256 _eventId) public view returns (string){
-        return Strings.strConcat(baseTokenURI(), "event=", Strings.uint2str(_eventId), "&ticket=", Strings.uint2str(_tokenId));
     }
 
 
@@ -38,7 +27,7 @@ contract Cryptotickets is TradeableERC721Token {
     *returns the base URI for the API endpoint (string)
     ************************************************************/
     function baseTokenURI() public view returns(string) {
-        return api_endpoint;
+        return "https://nuefwqsdv3.execute-api.us-east-1.amazonaws.com/testing/cryptotickets/";
     }
 
 
