d7fd7e81a4ada835775bec197c7ecbe952325879
fboydc <fboydc@gmail.com>
2019-01-14 14:05:58 -0500

    latest

--- backend/server/index.js
+++ backend/server/index.js
@@ -8,6 +8,8 @@ const abi = require('./src/ticketsfactory.json')
 const api = require('./meta/api.json')
 const bodyParser = require('body-parser')
 const uuidv1 = require('uuid/v1')
+const contract_address = "0x90d368d3b588ab4a37ab10d92077ec8fc9cfe51d";
+const privateKey= Buffer.from("4eb64b9546be3170b43733c71ec45da205c79c32cb99f58c89ccb8f91834867f", "hex")
 
 
 const server_wallet = "0x5824a273379D7c2960519101a43067eCfFF248a3"
@@ -17,8 +19,8 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 
-web3js = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/89003dae7ccd446d906c2196189ebfe3"));
-const contract = new web3js.eth.Contract(abi, "0x90d368d3b588ab4a37ab10d92077ec8fc9cfe51d");
+web3js = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/89003dae7ccd446d906c2196189ebfe3'));
+const contract = new web3js.eth.Contract(abi, contract_address);
 
 app.get('/get/:eventid', (req, res)=>{
     const {eventid} = req.params;
@@ -50,8 +52,15 @@ app.post('/add', (req, res)=>{
         if(data.body === true){
             console.log("we add to contract here");
             contract.methods.numOptions().call().then(num=>{
-                console.log("num", num);
-                contract.methods.createEvent(id, num);
+                web3js.eth.getTransactionCount(server_wallet).then(function(count){
+                    var rawTransaction = {"from":server_wallet, "gasPrice":web3js.utils.toHex(20*1e9), "gasLimit":web3js.utils.toHex(210000), "to":contract_address, "value":"0x0", "data":contract.methods.createEvent(id, num).encodeABI(), "nonce":web3js.utils.toHex(count)}
+                    var transaction = new tx(rawTransaction);
+                    transaction.sign(privateKey);
+                    web3js.eth.sendSignedTransaction('0x'+transaction.serialize().toString('hex')).on('transactionHash', ()=>{
+                        res.send("created event - " + id);
+                    });
+                    
+                });
             })
         }else{
             res.send(false);
@@ -60,6 +69,10 @@ app.post('/add', (req, res)=>{
 
 
 })
+
+
+
+
 
 
 
